0f680225c1b8e8fcd7806dc65d6e289fd812f37e
devekar <vaibhav.devekar@gmail.com>
2013-11-20 16:50:55 -0500

    sampling correction

--- sample.py
+++ sample.py
@@ -8,7 +8,7 @@ def split_dataset(test_percent, dataset):
 	dataset_len = len(dataset)
 	test_set_len = test_percent * dataset_len // 100
 	for i in xrange(test_set_len):
-		random_index = random.randint(0, test_set_len - i)
+		random_index = random.randint(0, dataset_len - i - 1)
 		test_set.append(dataset.pop(random_index))
 	return dataset, test_set
 
