fe68ebe7434fcab8850bce9225fbe179812cf902
akshay machhindra nikam <nikam@ri.cse.ohio-state.edu>
2013-11-26 01:46:23 -0500

    New results

---
+++ algo1/RESULTS_bk/plots/gnuplot.sh
@@ -0,0 +1,20 @@
+#
+# $Id: surface1.dem,v 1.11 2004/09/17 05:01:12 sfeam Exp $
+#
+
+
+set xlabel "Support"
+set ylabel "Confidence"
+set zlabel "Time"
+set title "Testing time in sec"
+set grid
+set dgrid 7,8
+#set dgrid 30, 30
+set pm3d
+set hidden3d
+set terminal png size 400,300
+set output "myplot.png"
+splot "graph_input.dat" every ::1 u 1:2:10 notitle with lines
+
+
+
